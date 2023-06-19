522e0eb496f3dccbc31e3eb86b7a01e9f0fa4eb9
PiechureQ <piechureq@wp.pl>
2023-06-19 11:26:01 +0200

    nvim tree and snippets update config

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -1,4 +1,7 @@
 local A = vim.api
+local function cmd_out(cmd)
+   return vim.api.nvim_cmd(vim.api.nvim_parse_cmd(cmd, {}), { output = true })
+end
 
 function CD()
    vim.cmd('cd %:p:h');
@@ -6,9 +9,23 @@ end
 
 A.nvim_create_user_command('CD', CD, {})
 
+function CDGit()
+   local init_path = cmd_out 'pwd';
+   CD()
+   local git_path = cmd_out 'Git rev-parse --show-toplevel'
+   if (string.find(git_path, 'fatal')) then
+      vim.cmd('cd ' .. init_path);
+   else
+      vim.cmd('cd ' .. git_path);
+   end
+end
+
+A.nvim_create_user_command('CDGit', CDGit, {})
+
 -- edit and load config file
 function OpenConfig()
    vim.cmd.tabe('$MYVIMRC')
    vim.cmd.CD()
 end
+
 A.nvim_create_user_command('OpenConfig', OpenConfig, {})
