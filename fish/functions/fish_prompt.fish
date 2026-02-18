c70853d79fb716b0a0f4332481f6a2053d4e780f
Michał Piechura <michal.piechura@bed-booking.com>
2026-02-18 09:09:12 +0100

    fix fish and fix for UHD monitors

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -220,7 +220,7 @@ function __theme_print_user
 end
 function __theme_print_time
     [ "$theme_display_time" = 'yes' ]; or return;
-    print_colored (command date $theme_display_time_format) $theme_color_time
+    print_colored " " (command date $theme_display_time_format) $theme_color_normal
 end
 function __theme_print_userhost
     echo -ns (__theme_print_superuser) (__theme_print_user) (__theme_reset_color)
